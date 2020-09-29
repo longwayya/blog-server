36ceb68fe5d3341a77255a8060a6fee7b20154ee
longwayya <longwayya@163.com>
2020-09-29 23:15:17 +0800

    详情接口实现

--- routes/articles.js
+++ routes/articles.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-const {getArticlelist} = require('../lib/mysql');
+const { getArticlelist, getArticledetail } = require('../lib/mysql');
 
 /* GET users listing. */
 router.get('/list', function (req, res, next) {
-  getArticlelist().then(resource => {
+  console.log(req.query.type)
+  getArticlelist(req.query.type).then(resource => {
     res.json(resource)
+  })
+});
+
+router.get('/detail', function (req, res, next) {
+  console.log(req.query)
+  getArticledetail(req.query.id).then(resource => {
+    res.json(resource[0])
   })
 });
 
